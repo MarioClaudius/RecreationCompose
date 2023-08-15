0a0b7f36c1828567d1c98d5f2afda493d10b7fa1
Mario Claudius <mcmario7901@gmail.com>
2023-08-15 17:19:20 +0700

    remove unused import

--- app/src/main/java/com/example/recreationcompose/MainActivity.kt
+++ app/src/main/java/com/example/recreationcompose/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.recreationcompose.ui.theme.RecreationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +17,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Blue//MaterialTheme.colorScheme.background
+                    color = Color.Blue
                 ) {
                     RecreationApp()
                 }
--- app/src/main/java/com/example/recreationcompose/ui/components/RecreationPlaceItem.kt
+++ app/src/main/java/com/example/recreationcompose/ui/components/RecreationPlaceItem.kt
@@ -1,19 +1,15 @@
 package com.example.recreationcompose.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,14 +19,11 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.recreationcompose.model.FakeRecreationPlaceDataSource
-import com.example.recreationcompose.model.RecreationPlace
 import com.example.recreationcompose.ui.theme.RecreationComposeTheme
 import com.example.recreationcompose.ui.theme.futuraFont
 
