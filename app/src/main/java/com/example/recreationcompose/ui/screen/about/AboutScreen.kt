c589e45433668379c37ffa9d16e064d6cb5b75cc
Mario Claudius <mcmario7901@gmail.com>
2023-08-15 12:52:52 +0700

    design about page

---
+++ app/src/main/java/com/example/recreationcompose/ui/screen/about/AboutScreen.kt
@@ -0,0 +1,70 @@
+package com.example.recreationcompose.ui.screen.about
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.recreationcompose.R
+import com.example.recreationcompose.ui.theme.RecreationComposeTheme
+import com.example.recreationcompose.ui.theme.futuraFont
+
+@Composable
+fun AboutScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foto_profil),
+            contentDescription = stringResource(id = R.string.creator_profile),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .clip(CircleShape)
+                .size(240.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.creator_name),
+            fontSize = 30.sp,
+            fontFamily = futuraFont,
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.creator_email),
+            fontSize = 20.sp,
+            fontFamily = futuraFont,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview() {
+    RecreationComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Cyan
+        ) {
+            AboutScreen()
+        }
+    }
+}
